90fe488089715ece6db3bf9fed5dbc7817802f09
unknown <kasatcka@ukr.net>
2020-02-12 21:53:51 +0200

    finish

--- myproj/src/js/app.js
+++ myproj/src/js/app.js
@@ -1,17 +1,17 @@
 $(function(){
-   var doc_h = $(window).height();
-   
-       $(window).scroll(function(){  
-        if ($(window).scrollTop() > doc_h/2)
-        {
-            $('#fixed').css('display', 'block');            
-        }
-        else{
-            $('#fixed').css('display', 'none');
-            };
-      } ); 
-      
-   $('#fixed').click(function(){
-       $(window).scrollTop(0);
-});                
+ var doc_h = $(window).height();
+
+ $(window).scroll(function(){
+    if ($(window).scrollTop() > doc_h/2)
+    {
+        $('#fixed').css('display', 'block');            
+    }
+    else{
+        $('#fixed').css('display', 'none');
+    };
+} );
+
+ $('#fixed').click(function(){
+     $(window).scrollTop(0);
+ });
 });
