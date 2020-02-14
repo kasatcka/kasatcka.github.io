54e1c4ca0c819b30961eaf4d8b62594f802e3cdf
unknown <kasatcka@ukr.net>
2020-02-14 21:27:44 +0200

    finaly projects

--- myproj/src/js/app.js
+++ myproj/src/js/app.js
@@ -4,7 +4,7 @@ $(function(){
  $(window).scroll(function(){
     if ($(window).scrollTop() > doc_h/2)
     {
-        $('#fixed').css('display', 'block');            
+        $('#fixed').css('display', 'block');
     }
     else{
         $('#fixed').css('display', 'none');
@@ -14,4 +14,12 @@ $(function(){
  $('#fixed').click(function(){
      $(window).scrollTop(0);
  });
+});
+
+
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+  box.classList.toggle('active');
 });
